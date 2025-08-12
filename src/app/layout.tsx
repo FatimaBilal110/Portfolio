be130ae73671c952a8104c1363cef256d859c2d0
Fatima Bilal <fatimabilal9012l@gmail.com>
2025-08-12 12:15:13 +0500

    Schema-graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,8 +41,36 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const schemaData = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    "name": "Fatima",
+    "url": "https://portfolio-gray-six-i0ldk2z83x.vercel.app/",
+    "image": "https://portfolio-gray-six-i0ldk2z83x.vercel.app/Images/profile.jpg",
+    "jobTitle": "Web Developer",
+    "description":
+      "Personal portfolio of Fatima — showcasing web development, design, and creative projects built with Next.js and Tailwind CSS.",
+    "sameAs": [
+      "https://github.com/FatimaBilal110"],
+    "knowsAbout": [
+      "Web Development",
+      "UI/UX Design",
+      "Next.js",
+      "Tailwind CSS",
+      "JavaScript"
+    ]
+  };
+
   return (
     <html lang="en">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(schemaData),
+          }}
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
